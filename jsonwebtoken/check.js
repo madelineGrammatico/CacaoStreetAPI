0684ef64bd5e5247b6b9df95a0e8797942407ad5
Madeline <grammaticomadeline@hotmail.fr>
2024-01-18 19:36:41 +0100

    fix(B02) :  fix bug

--- jsonwebtoken/check.js
+++ jsonwebtoken/check.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const extractBearer = authorization => {
-    if(typeof authirization !== 'string') {
+    if(typeof authorization !== 'string') {
         return false
     }
     const matches = authorization.match(/(bearer)\s+(\S+)/i)
@@ -11,9 +11,8 @@ const extractBearer = authorization => {
 
 const checkTokenMiddleware = (req, res, next) => {
     const token= req.headers.authorization && extractBearer(req.headers.authorization)
-
     if(!token){
-        return res.status(401).json({ message: "arhhhhhhhhhhhg"})
+        return res.status(401).json({ message: "arhhhhhhhhhhhg where are your token ?!?!!"})
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
