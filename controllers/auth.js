a73f81f5a102aa9a2b5709d6a65461718b8cb35c
Madeline <grammaticomadeline@hotmail.fr>
2024-01-24 15:49:02 +0100

    fix(B02) : refactoringauth's controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,31 +4,34 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 
 
-exports.loginUser = (req, res) => {
+exports.loginUser = async (req, res) => {
     const {email, password} = req.body
     if(!email || !password) {
         return res.status(400).json({message: "Bad email or password"})
     }
 
-    User.findOne({ where: {email: email}, raw: true})
-        .then(user => {
-            if (user === null) {
-                return res.status(401).json({message: "This account does not exists !"})
-            }
-            bcrypt.compare(password, user.password)
-                .then(test => {
-                    if(!test) {
-                        return res.status(401).json({message: "Wrong password"})
-                    }
+    try {
+        const user = await User.findOne({ where: {email: email}, raw: true})
+        if (user === null) {
+            return res.status(401).json({message: "This account does not exists !"})
+        }
 
-                    const token = jwt.sign({
-                         id: user.id,
-                         speudo: user.speudo,
-                         email: user.email
-                    }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_DURING})
+        const test = await bcrypt.compare(password, user.password)
+        if(!test) {
+            return res.status(401).json({message: "Wrong password"})
+        }
 
-                    return res.json({access_token: token})
-                })
-                .catch( err => res.status(500).json({message: "Login process failed, error: err"}))
-        })
+        const token = jwt.sign({
+            id: user.id,
+            speudo: user.speudo,
+            email: user.email
+       }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_DURING})
+
+       return res.json({access_token: token})
+    } catch (err) {
+        if (err.name ==='SequelizeDataBaseError') {
+            res.status(500).json({message: "DataBase Error", error: err})
+        }
+        res.status(500).json({message: "Login process failed", error: err})
+    }
 }
