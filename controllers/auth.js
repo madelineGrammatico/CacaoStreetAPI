29950569537d5de1921c146a63f163222421a437
Madeline <grammaticomadeline@hotmail.fr>
2024-01-31 16:29:56 +0100

    fix(B02) : add UserError to user's controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,23 +2,29 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const UserError = require('../errors/UserError')
 
 
 exports.loginUser = async (req, res) => {
-    const {email, password} = req.body
-    if(!email || !password) {
-        return res.status(400).json({ message: "Bad email or password" })
-    }
+    
 
     try {
+        const {email, password} = req.body
+        if(!email || !password) {
+            // return res.status(400).json({ message: "Bad email or password" })
+            throw new UserError("Bad email or password")
+        }
+
         const user = await User.findOne({ where: {email: email}, raw: true})
         if (user === null) {
-            return res.status(401).json({ message: "This account does not exists !" })
+            // return res.status(401).json({ message: "This account does not exists !" })
+            throw new UserError("This account does not exists !")
         }
 
         const test = await bcrypt.compare(password, user.password)
         if(!test) {
-            return res.status(401).json({ message: "Wrong password" })
+            // return res.status(401).json({ message: "Wrong password" })
+            throw new UserError("Wrong password")
         }
 
         const token = jwt.sign({
@@ -32,6 +38,6 @@ exports.loginUser = async (req, res) => {
         if (err.name ==='SequelizeDataBaseError') {
             res.status(500).json({ message: "DataBase Error", error: err })
         }
-        res.status(500).json({ message: "Login process failed", error: err })
+        res.status(err.statusCode || 500).json({ message: err.message, error: err})
     }
 }
