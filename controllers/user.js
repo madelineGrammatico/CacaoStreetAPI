1d40814ef63b15717c6cfe86019718689a598366
Madeline <grammaticomadeline@hotmail.fr>
2024-01-31 16:28:12 +0100

    fix(B02) : add UserError to auth's controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
-
+const UserError = require('../errors/UserError')
 
 exports.getAllUsers = (req, res) => {
     User.findAll()
@@ -12,40 +12,40 @@ exports.getAllUsers = (req, res) => {
 }
 
 exports.getUser = async (req, res) => {
-    
-
     try {
         const userId = parseInt(req.params.id)
-
         if(!userId) {
             // return res.json(400).jsonp({ message: "Missing Parameter"})
-            throw new Error("Missing Parameter")
+            throw new UserError("Missing Parameter")
         }
+
         const user = await User.findOne({ where: {id: userId}, raw: true })
-
         if((user === null)) {
-            return res.status(404).json({ message: "This user does not exist !"})
+            // return res.status(404).json({ message: "This user does not exist !"})
+            throw new UserError("This user does not exist !")
         }
-
         return res.json({data: user})
 
     } catch(err) {
-        res.status(500).json({ message: 'DataBase Error', error: err})
+        res.status(err.statusCode || 500).json({ message: err.message, error: err})
     }
 }
 
 exports.addUser = async (req, res) => {
-    const {speudo, email, password } = req.body
-
-    if(!speudo || !email || !password) {
-        return res.status(400).json({ message: "Missing Data" })
-    }
+    
 
     try {
+        const {speudo, email, password } = req.body
+        if(!speudo || !email || !password) {
+            // return res.status(400).json({ message: "Missing Data" })
+            throw new UserError("Missing Data")
+        }
+
         const user = await User.findOne({where: { email: email}, raw: true})
 
         if(user !== null) {
-            return res.status(409).json({ message: `The email ${email} already exists`})
+            // return res.status(409).json({ message: `The email ${email} already exists`})
+            throw new UserError(`The email ${email} already exists`)
         }
 
         const hash = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SALT_ROUND))
@@ -55,28 +55,31 @@ exports.addUser = async (req, res) => {
         return res.json({message: "UserCreated", data: userCrypted})
         
     }catch(err) {
-        res.status(500).json({ message: 'DataBase Error', error: err })
+        res.status(err.statusCode || 500).json({ message: err.message, error: err})
     }
 }
 
 exports.updateUser = async (req, res) => {
-    let userId = parseInt(req.params.id)
-
-    if(!userId) {
-        return res.status(400).json({ message: "Missing parameter" })
-    }
+    
     try {
+        let userId = parseInt(req.params.id)
+        if(!userId) {
+            // return res.status(400).json({ message: "Missing parameter" })
+            throw new Error("Missing parameter" )
+        }
+
         const user = await User.findOne({ where: {id: userId}, raw: true})
 
         if(user === null) {
-            return res.status(404).json({ message: "This user does not exist !"})
+            // return res.status(404).json({ message: "This user does not exist !"})
+            throw new Error("This user does not exist !")
         }
         
         await User.update(req.body, {where: {id: userId}})
         return res.json({ message: "User Updated", data: user })
 
     } catch(err) {
-        res.status(500).json({ message: 'DataBase Error', error: err })
+        res.status(err.statusCode || 500).json({ message: err.message, error: err})
     }
 }
 
