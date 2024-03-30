134ad4e42ed1d1d79e39b853687fecb92f623057
Madeline <grammaticomadeline@hotmail.fr>
2024-03-30 10:42:06 +0100

    remove useless comments

--- models/role.js
+++ models/role.js
@@ -11,20 +11,3 @@ module.exports = (sequelize, Sequelize) => {
   
     return Role
   }
-
-// const { DataTypes } = require('sequelize')
-// // const DB = require("../db.config")
-// module.exports = (sequelize) => {
-//     const Role = sequelize.define("Role", {
-//         id: {
-//             type: DataTypes.INTEGER(11),
-//             primaryKey: true,
-//             autoIncrement: true
-//         },
-//         name: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         }
-//     })
-//     return Role
-// }
